bc2ea474014234c6c638d35dd5104b35dbedbd31
Glitch (eastern-scandalous-taste) <none>
2021-12-29 15:49:30 +0000

    📙👄 Checkpoint ./package.json:39277590/296 ./db/db.js:39277590/1033

--- db/db.js
+++ db/db.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 
-mongoose.connect('mongodb://localhost:27017/ecomm').then((done) => {
-    console.log('connected')
-}).catch((e) =>{
-    console.log(e)
-})
+const conn = mongoose.connect('mongodb://asim:Mardan8110@cluster0-shard-00-00.btwlh.mongodb.net:27017,cluster0-shard-00-01.btwlh.mongodb.net:27017,cluster0-shard-00-02.btwlh.mongodb.net:27017/voting_app?ssl=true&replicaSet=atlas-x564yd-shard-0&authSource=admin&retryWrites=true&w=majority').then(()=> {
+    console.log('db connected');
 
+}).catch((e) => {
+    console.log(e);
+    console.log('catch errorr');
+})
 
 export default mongoose
